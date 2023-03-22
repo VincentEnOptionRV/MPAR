af51a87897929f431bd9e96c7b7633d7fbc19972
antNLocks <antn.lorentz@gmail.com>
2023-03-22 14:05:14 +0100

    Max

--- MDP-parser/modelChecking.py
+++ MDP-parser/modelChecking.py
@@ -2,6 +2,9 @@ from mdp import MDP
 import numpy as np
 from scipy.sparse.csgraph import connected_components
 from numpy.linalg import solve
+from numpy.linalg import lstsq
+from scipy.optimize import linprog
+
 
 def buildAdversary(mdp:MDP):
     print("Build Adversary")
@@ -55,7 +58,8 @@ def modelChecking(mdp:MDP, adversary, states, n=None):
             b[i] += mc[S_unknown[i]][j]
 
     if n is None:
-        proba = solve(np.eye(len(S_unknown)) - A, b)
+        try: proba = solve(np.eye(len(S_unknown)) - A, b)
+        except: proba = lstsq(np.eye(len(S_unknown)) - A, b, rcond=None)[0]
 
         if len(states) > 1: print(f'Prop = <>({" v ".join(states)})')
         else: print(f'Prop = <>{states[0]}')
@@ -76,6 +80,45 @@ def modelChecking(mdp:MDP, adversary, states, n=None):
         else:
             print(f'P({mdp.states[i_state]:{maxStateLen}}|= Prop) = {proba[S_unknown.index(i_state)]:6.1%}')
 
+
+def modelCheckingMaxMin(mdp:MDP, states):
+    i_states = [np.where(mdp.states == state)[0][0] for state in states]
+    
+    A = np.zeros((len(mdp.states) * len(mdp.actions), len(mdp.states)))
+    b = np.zeros(len(mdp.states) * len(mdp.actions))
+
+    for i_s in range(len(mdp.states)):
+        for i_a in range(len(mdp.actions)):
+            for j_s in range(len(mdp.states)):
+                if i_s == j_s: aij = mdp.P[i_a][i_s][j_s] - 1
+                else: aij = mdp.P[i_a][i_s][j_s]
+                A[i_s * len(mdp.actions) + i_a][j_s] = aij
+
+    for i_s in range(len(mdp.states)):
+        for i_a in range(len(mdp.actions)):
+            for j_s in i_states:
+                b[i_s * len(mdp.actions) + i_a] -= mdp.P[i_a][i_s][j_s]
+
+    for i_s in i_states:
+        for j_s in range(len(mdp.states)):
+            for i_a in range(len(mdp.actions)):
+                A[i_s * len(mdp.actions) + i_a][j_s] = 0
+
+    for i_s in i_states:
+        for i_a in range(len(mdp.actions)):
+            b[i_s * len(mdp.actions) + i_a] = 0
+
+    res = linprog(c=np.ones(len(mdp.states)), A_ub=A, b_ub=b, bounds=(0,1))
+
+    if len(states) > 1: print(f'Prop = <>({" v ".join(states)})')
+    else: print(f'Prop = <>{states[0]}')
+
+    maxStateLen = max([len(s) for s in mdp.states])
+    for i_state in range(len(mdp.states)):
+        if i_state in i_states:
+            print(f'Pmax({mdp.states[i_state]:{maxStateLen}}|= Prop) = 100.0%')
+        else:
+            print(f'Pmax({mdp.states[i_state]:{maxStateLen}}|= Prop) = {res.x[i_state]:6.1%}')
 
 
 def _calcS0S1(mc, i_state):
